f78b6ea99b2e57615f109f34d17d7da7803815f7
Lakshay-coder001 <Lakshay.in0001@gmail.com>
2025-05-01 12:42:27 +0530

    exam route

--- Campus_copilot/Campus_copilot/urls.py
+++ Campus_copilot/Campus_copilot/urls.py
@@ -4,6 +4,6 @@ from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_views.home, name='home'),  # Default landing page
-    path('exam-calendar/', include('exam_calender.urls')),  # Exam calendar URLs
+    path('', include('home.urls')),
+    path('exam-calendar/', include('exam_calender.urls')),
 ]
